185f63b83f2bc93edb368ec6cb70537f591a492b
sales@unlockai.in <sales@unlockai.in>
2018-09-06 19:32:10 +0530

    * Method.cs: Added changes to create Method table using Code first
    * MethodDBContext.cs:

---
+++ DataDecipher.WebApp/Data/MethodDBContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace DataDecipher.WebApp.Data
+{
+    public class MethodDBContext : DbContext
+    {
+        IConfiguration configuration;
+        public MethodDBContext(IConfiguration config) 
+        {
+            configuration = config;
+        }
+        public DbSet<Method> Methods { get; set; }
+      
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+        }
+    }
+}
