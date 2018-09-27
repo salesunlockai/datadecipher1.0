c5ed87503f13f53a768bfe739c3a607e6577eefe
Deepmala Gupta <sales@unlockai.in>
2018-09-27 04:18:47 -0700

    Added screens for Organization and Plan

---
+++ DataDecipher.WebApp/Controllers/PlanController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DataDecipher.WebApp.Data;
+using DataDecipher.WebApp.Models;
+
+namespace DataDecipher.WebApp.Controllers
+{
+    public class PlanController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PlanController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Plan
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Plans.ToListAsync());
+        }
+
+        // GET: Plan/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var plan = await _context.Plans
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (plan == null)
+            {
+                return NotFound();
+            }
+
+            return View(plan);
+        }
+
+        // GET: Plan/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Plan/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,TrialPeriod,Price")] Plan plan)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(plan);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(plan);
+        }
+
+        // GET: Plan/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var plan = await _context.Plans.FindAsync(id);
+            if (plan == null)
+            {
+                return NotFound();
+            }
+            return View(plan);
+        }
+
+        // POST: Plan/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Id,Name,TrialPeriod,Price")] Plan plan)
+        {
+            if (id != plan.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(plan);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PlanExists(plan.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(plan);
+        }
+
+        // GET: Plan/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var plan = await _context.Plans
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (plan == null)
+            {
+                return NotFound();
+            }
+
+            return View(plan);
+        }
+
+        // POST: Plan/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var plan = await _context.Plans.FindAsync(id);
+            _context.Plans.Remove(plan);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PlanExists(string id)
+        {
+            return _context.Plans.Any(e => e.Id == id);
+        }
+    }
+}
