1562d090b3f670d18d75215e1652d6f2182bcab1
salesunlockai <sales@unlockai.in>
2018-11-06 23:48:43 -0500

    * DataDecipher.WebApp.csproj: * MainController.cs: Added functionality for Txt, Dat and Xml files
    * _ParsingRulesTxtDat.cshtml: Added functionality for Txt, Dat files
    
    * _ParsingRulesXml.cshtml: Added functionality for Xml files

--- DataDecipher.WebApp/Controllers/MainController.cs
+++ DataDecipher.WebApp/Controllers/MainController.cs
@@ -28,7 +28,14 @@ namespace DataDecipher.WebApp.Controllers
         [HttpPost]
         public IActionResult DisplayParsingViewTxtDat(string filePath)
         {
-            return PartialView("_ParsingRulesTxtDat");
+            var model1 = new RawData
+            {
+                fileName = Path.GetFileName(filePath),
+                filePath = filePath
+            };
+            model1.rawData = model1.GetRawData(model1.filePath);
+
+            return PartialView("_ParsingRulesTxtDat", model1);
         }
 
         [HttpPost]
@@ -47,7 +54,14 @@ namespace DataDecipher.WebApp.Controllers
         [HttpPost]
         public IActionResult DisplayParsingViewXml(string filePath)
         {
-            return PartialView("_ParsingRulesXml");
+            var model1 = new RawData
+            {
+                fileName = Path.GetFileName(filePath),
+                filePath = filePath
+            };
+            model1.rawData = model1.GetRawData(model1.filePath);
+
+            return PartialView("_ParsingRulesXml", model1);
         }
 
         [HttpPost]
@@ -104,66 +118,97 @@ namespace DataDecipher.WebApp.Controllers
         }
 
         [HttpPost]
-        public IActionResult DisplayParsedTxtDatFile(string inputSelectedFile, string recordStart, string recordEnd, string headerStart, string headerEnd, string tableStart, string tableEnd, string tableColumns)
+        public IActionResult DisplayParsedTxtDatFile(string inputSelectedFile, 
+                                                     string recordMarkerStartText,
+                                                     string recordMarkerEndText,
+                                                     string tableMarkerStartText,
+                                                     string tableMarkerEndText,
+                                                     string headerMarkerStartText,
+                                                     string headerMarkerEndText,
+                                                     string headerFieldsSelection,
+                                                     string tableFieldsSelection,
+                                                     string delimiter)
         {
+            string[] headerFields = headerFieldsSelection.Split(',');
+            string[] tableFields = tableFieldsSelection.Split(',');
             var model1 = new ParsedData
             {
                 filePath = inputSelectedFile,
                 fileName = Path.GetFileName(inputSelectedFile)
             };
 
-            string fileExtension = Path.GetExtension(inputSelectedFile);
-            if (fileExtension == ".csv")
+            //string parsingRules = "{\"funcInput\":{\"recordMarkerStartText\" : \"Software Version\",\"recordMarkerEndText\" : \"XXX Laboratories\",\"tableMarkerStartText\" : \"Water Report\",\"tableMarkerEndText\" : \"Timed Event Table\",\"headerMarkerStartText\" : \"Software Version\",\"headerMarkerEndText\" : \"Water Report\",\"headerFieldsSelection\" : [\"Sample Name\",\"Sample Number\",\"Instrument\",\"Sample Amount\"],\"tableFieldsSelection\" : [\"Time\",\"Component\",\"Adj Amt\",\"Area\"],\"delimiter\" : \",\"}}";
+            string parsingRules = "{\"funcInput\":{\"recordMarkerStartText\":\"" + recordMarkerStartText
+                                    + "\",\"recordMarkerEndText\":\"" + recordMarkerEndText
+                                    + "\",\"tableMarkerStartText\":\"" + tableMarkerStartText
+                                    + "\",\"tableMarkerEndText\":\"" + tableMarkerEndText
+                                    + "\",\"headerMarkerStartText\":\"" + headerMarkerStartText
+                + "\",\"headerMarkerEndText\" : \"" + headerMarkerEndText + "\",\"headerFieldsSelection\":[\"";
+            for (int i = 0; i < headerFields.Length; i++)
             {
-                string parsingRules = "{\"funcInput\":{\"attribute_list\" : [\"Testfile\",\"SampleName\",\"ExtCal.Average\",\"iCapOES\"],\"delimiter\" : \";\"  }}";
-                //REST Call
-                var client = new RestClient("https://fileparserapp-new.appspot.com/FileParser/CSV");
-                var request = new RestRequest();
-                request.Method = RestSharp.Method.POST;
-                request.JsonSerializer.ContentType = "multipart/form-data";
-                request.Parameters.Clear();
-                request.AddFile("ParsingFile", model1.filePath); // adds to POST or URL querystring based on Method
-                request.AddParameter("ParsingRules", parsingRules);
-                IRestResponse response = client.Execute(request);
-                model1.parsedData = response.Content; // raw content as string
-                model1.parsedDataTable = model1.GetParsedDataTable(model1.parsedData);
+                if (i < (headerFields.Length - 1))
+                    parsingRules = parsingRules + headerFields[i] + "\",\"";
+                else parsingRules = parsingRules + headerFields[i] + "\"],\"tableFieldsSelection\":[\"";
             }
-            else if (fileExtension == ".xml")
+            for (int i = 0; i < tableFields.Length; i++)
             {
-                string parsingRules = "{\"funcInput\": {\"parentTag\": \"TestOrder\",\"headerFields\": [\"TestOrder:Id\",\"TestOrder:LastModificationActorId\",\"TestOrder:Specimen\"],\"tableFields\": [\"TestOrder:TestOrderDate\",\"TestOrder:Status\",\"TestData:Id\"]}}";
-                //REST Call
-                var client = new RestClient("https://fileparserapp-new.appspot.com/FileParser/XML");
-                var request = new RestRequest();
-                request.Method = RestSharp.Method.POST;
-                request.JsonSerializer.ContentType = "multipart/form-data";
-                request.Parameters.Clear();
-                request.AddFile("ParsingFile", model1.filePath); // adds to POST or URL querystring based on Method
-                request.AddParameter("ParsingRules", parsingRules);
-                IRestResponse response = client.Execute(request);
-                model1.parsedData = response.Content; // raw content as string
-                model1.parsedDataTable = model1.GetParsedDataTable(model1.parsedData);
+                if (i < (tableFields.Length - 1))
+                    parsingRules = parsingRules + tableFields[i] + "\",\"";
+                else parsingRules = parsingRules + tableFields[i] + "\"],\"";
             }
-            else if (fileExtension == ".txt" || fileExtension == ".dat")
+            parsingRules = parsingRules + "delimiter\":\"" + delimiter + "\"}}";
+
+            //REST Call
+            var client = new RestClient("https://fileparserapp-new.appspot.com/FileParser/TXT");
+            var request = new RestRequest();
+            request.Method = RestSharp.Method.POST;
+            request.JsonSerializer.ContentType = "multipart/form-data";
+            request.Parameters.Clear();
+            request.AddFile("ParsingFile", model1.filePath); // adds to POST or URL querystring based on Method
+            request.AddParameter("ParsingRules", parsingRules);
+            IRestResponse response = client.Execute(request);
+            model1.parsedData = response.Content; // raw content as string
+            model1.parsedDataTable = model1.GetParsedDataTable(model1.parsedData);
+            return PartialView("_ParsedData", model1);
+        }
+
+        [HttpPost]
+        public IActionResult DisplayParsedXmlFile(string inputSelectedFile, string parentTag, string headerFields, string tableFields)
+        {
+            string[] listHeaderFields = headerFields.Split(',');
+            string[] listTableFields = tableFields.Split(',');
+            var model1 = new ParsedData
             {
-                string parsingRules = "{\"funcInput\":{\"recordMarkerStartText\" : \"Software Version\",\"recordMarkerEndText\" : \"XXX Laboratories\",\"tableMarkerStartText\" : \"Water Report\",\"tableMarkerEndText\" : \"Timed Event Table\",\"headerMarkerStartText\" : \"Software Version\",\"headerMarkerEndText\" : \"Water Report\",\"headerFieldsSelection\" : [\"Sample Name\",\"Sample Number\",\"Instrument\",\"Sample Amount\"],\"tableFieldsSelection\" : [\"Time\",\"Component\",\"Adj Amt\",\"Area\"],\"delimiter\" : \",\"}}";
-                //REST Call
-                var client = new RestClient("https://fileparserapp-new.appspot.com/FileParser/TXT");
-                var request = new RestRequest();
-                request.Method = RestSharp.Method.POST;
-                request.JsonSerializer.ContentType = "multipart/form-data";
-                request.Parameters.Clear();
-                request.AddFile("ParsingFile", model1.filePath); // adds to POST or URL querystring based on Method
-                request.AddParameter("ParsingRules", parsingRules);
-                IRestResponse response = client.Execute(request);
-                model1.parsedData = response.Content; // raw content as string
-                model1.parsedDataTable = model1.GetParsedDataTable(model1.parsedData);
+                filePath = inputSelectedFile,
+                fileName = Path.GetFileName(inputSelectedFile)
+            };
+
+            //string parsingRules = "{\"funcInput\": {\"parentTag\": \"TestOrder\",\"headerFields\": [\"TestOrder:Id\",\"TestOrder:LastModificationActorId\",\"TestOrder:Specimen\"],\"tableFields\": [\"TestOrder:TestOrderDate\",\"TestOrder:Status\",\"TestData:Id\"]}}";
+            string parsingRules = "{\"funcInput\":{\"parentTag\":\"" + parentTag + "\",\"headerFields\":[\"";
+            for (int i = 0; i < listHeaderFields.Length; i++)
+            {
+                if (i < (listHeaderFields.Length - 1))
+                    parsingRules = parsingRules + listHeaderFields[i] + "\",\"";
+                else parsingRules = parsingRules + listHeaderFields[i] + "\"],\"tableFields\":[\"";
             }
-            else
+            for (int i = 0; i < listTableFields.Length; i++)
             {
-                model1.parsedData = "File Format Not Supported :( \r\n No Data To Display";
-                model1.parsedDataTable = model1.GetParsedDataTable(model1.parsedData);
+                if (i < (listTableFields.Length - 1))
+                    parsingRules = parsingRules + listTableFields[i] + "\",\"";
+                else parsingRules = parsingRules + listTableFields[i] + "\"]}}";
             }
 
+            //REST Call
+            var client = new RestClient("https://fileparserapp-new.appspot.com/FileParser/XML");
+            var request = new RestRequest();
+            request.Method = RestSharp.Method.POST;
+            request.JsonSerializer.ContentType = "multipart/form-data";
+            request.Parameters.Clear();
+            request.AddFile("ParsingFile", model1.filePath); // adds to POST or URL querystring based on Method
+            request.AddParameter("ParsingRules", parsingRules);
+            IRestResponse response = client.Execute(request);
+            model1.parsedData = response.Content; // raw content as string
+            model1.parsedDataTable = model1.GetParsedDataTable(model1.parsedData);
             return PartialView("_ParsedData", model1);
         }
     }
